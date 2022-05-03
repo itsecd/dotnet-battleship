bff83e95078c7bb8953ec51f93bb283baae7328c
Ruslan Yuzkiv <55447008+navna@users.noreply.github.com>
2022-05-03 15:20:54 +0400

    Develop protocol requests

---
+++ Battleship.ConsoleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+using Battleship.Api;
+
+using Grpc.Net.Client;
+
+namespace Battleship.ConsoleClient
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new BattleshipService.BattleshipServiceClient(channel);
+            var stream = client.Connect();
+
+            await stream.RequestStream.WriteAsync(CreateLogin("Ivan"));
+            await stream.RequestStream.WriteAsync(CreateFindOpponent());
+            await stream.RequestStream.CompleteAsync();
+            await Task.Delay(1000);
+        }
+
+        private static Request CreateLogin(string login)
+        {
+            var loginRequest = new LoginRequest {Login = login};
+            return new Request {Login = loginRequest};
+        }
+
+        private static Request CreateFindOpponent()
+        {
+            var findOpponentRequest = new FindOpponentRequest();
+            return new Request {FindOpponent = findOpponentRequest};
+        }
+    }
+}
---
+++ Battleship.Server/Services/BattleshipService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+
+using Battleship.Api;
+
+using Grpc.Core;
+
+namespace Battleship.Server.Services
+{
+    public class BattleshipService : Api.BattleshipService.BattleshipServiceBase
+    {
+        public override async Task Connect(
+            IAsyncStreamReader<Request> requestStream,
+            IServerStreamWriter<Reply> responseStream,
+            ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine(requestStream.Current);
+            }
+        }
+    }
+}
