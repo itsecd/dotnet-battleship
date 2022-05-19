48aa11b0a90bb505048958105bee60453d57ac2b
Ruslan Yuzkiv <yuzkiv@geosamara.ru>
2022-05-19 14:27:00 +0400

    Add MessageWindow

---
+++ Battleship.Client/Views/MessageWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Battleship.Client.Views
+{
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
